8ce12b8f947f243b4a8e3a941b83f18839f39c5f
TonyJem <a.jemanov@gmail.com>
2021-07-03 17:33:18 +0300

    Revert "Minor polishing refactoring"
    This reverts commit 202833e7f629b1c3db07153167cacc1e94038cd2.

--- GaleryView2/GaleryView2/ImageCollectionViewController.swift
+++ GaleryView2/GaleryView2/ImageCollectionViewController.swift
@@ -5,12 +5,12 @@ final class ImageCollectionViewController: UICollectionViewController {
     @IBOutlet private weak var searchTextField: UITextField!
     @IBOutlet private weak var clearSearchButton: UIBarButtonItem!
     
-    private let flickr = Flickr()
-    private let itemsPerRow: CGFloat = 3
     private let reuseIdentifier = "ImageCell"
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    
     private var searchResult = FlickrSearchResult(searchText: "", filteredImages: [])
+    private let flickr = Flickr()
+    private let itemsPerRow: CGFloat = 3
+    
     private var selectedIndexPath = IndexPath()
     
     override func viewDidLoad() {
@@ -78,6 +78,7 @@ extension ImageCollectionViewController: UITextFieldDelegate {
 
 // MARK: - CollectionView DataSource
 extension ImageCollectionViewController {
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return searchResult.filteredImages.count
     }
@@ -110,6 +111,7 @@ extension ImageCollectionViewController {
 
 // MARK: - Collection View Flow Layout Delegate
 extension ImageCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
         let availableWidth = view.frame.width - paddingSpace
